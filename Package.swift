a9a13db01ff1a929686fc86c4cd68fad3e261891
Bruno Wernimont <bruno.wernimont@gmail.com>
2019-10-22 17:12:14 +0200

    Add swift package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Mocker",
+    platforms: [
+        .iOS(.v7),
+        .macOS(.v10_9),
+        .tvOS(.v9),
+        .watchOS(.v2),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Mocker",
+            targets: ["Mocker"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Mocker",
+            dependencies: [],
+            path: "Sources"),
+    ],
+)
